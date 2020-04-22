d0755f803351e8b6dba598d51a4a0eff1cf2db67
jordan h <jordanhiatt@u.boisestate.edu>
2020-04-22 23:09:27 +0000

    testing Account subclass, should work as example

--- Account.swift
+++ Account.swift
@@ -2,12 +2,21 @@
 //subclass will be like class CheckingAccount : Account
 //and the funcs that need to be overwritten will use "override"
 public class Account {
+    //in order for the properties to be inherited, they can't be private? 
+    //does internal have enouh protection? 
+
     //needs initializers to run
-    private var number = 1;
+    internal var number: Int;
     //private var customer: Customer;
-    private var balance = 0.0;
+    internal var balance: Double;
+    //init is the constructor
+    init(number:Int, balance: Double){
+      self.number = number
+      self.balance = balance
+    }
 
     public func accrue(rate: Double) -> (){};
+    //public func accrue(){};
 
     //can't have the same name as private variable
     func accBalance() -> Double {
@@ -26,12 +35,32 @@ public class Account {
 	    //return number+":"+customer+":"+balance;
       //the string system makes it very easy to add variables
       //to the toString. No "addition" of strings.
-      return "\(number):\(balance)"
+      return "\(number) : \(balance)"
     }
 }
 
-var acc = Account()
+public class AccountSub : Account {
+  private var interest  = 0.0;
+  //overriding instance method must be as accessible as the declaration it overrides
+  override init(number:Int, balance: Double){
+    // self.number = number
+    // self.balance = balance
+    super.init(number: number, balance:balance)
+  }
+  public override func accrue(rate: Double) ->(){
+    	interest+=balance*rate;
+	    balance+=balance*rate;
+  }
+}
+
+var acc = Account(number: 1, balance: 50)
+var accSub = AccountSub(number:1 ,balance:30)
+print(accSub.toString())
+accSub.accrue(rate:32.0)
+
 //parameters require names
-acc.deposit(amount: 25)
-print(acc.toString())
+// acc.deposit(amount: 25)
+// acc.withdraw(amount: 10)
+// print(acc.accBalance())
+// print(acc.toString())
 
